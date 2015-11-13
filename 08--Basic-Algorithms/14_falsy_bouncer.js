d75ad41d1ea8bf99dae381bacf81c52f56bb2cb4
Jeff Stautz <jeff.stautz@gmail.com>
2015-11-12 23:55:08 -0800

    Falsy Bouncer solution

---
+++ 08--Basic-Algorithms/14_falsy_bouncer.js
@@ -0,0 +1,23 @@
+/*
+ * Free Code Camp
+ * 08 - 14 - Falsy Bouncer
+ *
+ * Created by Jeff Stautz, 2015-11-12
+ *
+ * Instructions:
+ *   Remove all falsy values from an array.
+ *   Falsy values in javascript are false, null, 0, "", undefined, and NaN.
+ *   Remember to use Read-Search-Ask if you get stuck. Write your own code.
+ *   Here are some helpful links:
+ *   Boolean Objects -- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+ *   Array.filter() -- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ */
+
+function isFalse(value) {
+    return value;
+}
+
+function bouncer(arr) {
+    return arr.filter(isFalse);
+}
+
