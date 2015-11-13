ba838862aa0f13b703f389881caba9d0613f09b2
Jeff Stautz <jeff.stautz@gmail.com>
2015-11-12 23:36:13 -0800

    Much better solution to Chunky Monkey
    Not sure why I didn't think of slicing the array before...

--- 08--Basic-Algorithms/11_chunky_monkey.js
+++ 08--Basic-Algorithms/11_chunky_monkey.js
@@ -14,32 +14,11 @@
 
 
 function chunk(arr, size) {
-  if (size > arr.length) {
-    return arr;
-  }
-  
-  var chunkedArray = [];
-  var chunk = [];
-  var chunkCount = 0;
-  
-  for (i = 0; i < arr.length; i++) {
-    if (chunkCount < size) {
-      chunk.push(arr[i]);
-      chunkCount++;
-    } 
-    // Otherwise, this chunk is done
-    else {
-      chunkedArray.push(chunk);
-      chunk = [];
-      chunk.push(arr[i]);
-      chunkCount = 1;
+    var chunkedArray = [];
+    for (i = 0; i < arr.length; i += size) {
+        chunkedArray.push(arr.slice(i, i + size));
     }
-  }
-  // Could get here with chunkCount still set if last bit is incomplete chunk
-  if (chunkCount > 0) {
-    chunkedArray.push(chunk);
-  }
-  return chunkedArray;
+    return chunkedArray;
 }
 
 chunk([0,1,2,3,4,5], 2);
