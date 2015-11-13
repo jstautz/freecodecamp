57c4a5cce8948e6a1453cab82285e32888814f46
Jeff Stautz <jeff.stautz@gmail.com>
2015-11-12 23:50:19 -0800

    Minor change to equality check on Mutations

--- 08--Basic-Algorithms/13_mutations.js
+++ 08--Basic-Algorithms/13_mutations.js
@@ -17,8 +17,9 @@
 function mutation(arr) {
     var first = arr[0].toLowerCase();
     var second = arr[1].toLowerCase();
+    
     for (i = 0; i < second.length; i++) {
-        if (first.indexOf(second[i]) == -1) {
+        if (first.indexOf(second[i]) === -1) {
             return false;
         }
     }
